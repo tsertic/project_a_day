925eae7e1f5a47d1cf231df025f6d5b6dc55f85a
tsertic <tsertic5@gmail.com>
2023-06-30 15:37:54 +0200

    project2: added cors

--- project2_express_node_api/server.js
+++ project2_express_node_api/server.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const app = express();
-
+const cors = require("cors");
+const usersRoutes = require("./routes/users");
 const PORT = process.env.PORT || 5500;
-
+app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("You get / route");
-});
+app.use("/users", usersRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server up and running on PORT:${PORT}`);
