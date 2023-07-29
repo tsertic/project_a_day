4866795435184ad6ed6893ec3f27b0c6c2d8fae7
Tomislav Sertic <tsertic5@gmail.com>
2023-07-29 20:36:37 +0200

    project3: added placeholder for routes and controllers

---
+++ project3_todos_api_express_mongodb/controllers/todos.js
@@ -0,0 +1,24 @@
+// GET
+// v1/todos
+// get all todos
+exports.getAllTodos = async (req, res) => {};
+
+// POST
+// v1/todos
+// create new todo
+exports.createTodo = async (req, res) => {};
+
+// GET
+// v1/todos/:id
+// get todo by id
+exports.getTodoById = async (req, res) => {};
+
+// PUT
+// v1/todos/:id
+// update todo by id
+exports.updateTodoById = async (req, res) => {};
+
+//DELETE
+// v1/todos/:id
+// delete todo by id
+exports.deleteTodoById = async (req, res) => {};
--- project3_todos_api_express_mongodb/server.js
+++ project3_todos_api_express_mongodb/server.js
@@ -1,8 +1,24 @@
 const express = require("express");
 const app = express();
-
+require("dotenv").config();
+require("colors");
+const morgan = require("morgan");
+//CONSTANTS
 const PORT = process.env.PORT || 5678;
+//MIDLEWARES
+app.use(express.json());
+if (process.env.NODE_ENV === "dev") {
+  console.log(morgan("dev"));
+}
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://127.0.0.1:${PORT}`);
+//ROUTES
+
+//SERVER
+const server = app.listen(PORT, () => {
+  console.log(`Server is running on http://127.0.0.1:${PORT}`.green);
+});
+
+process.on("unhandledRejection", (err, promise) => {
+  console.error(`ERROR: ${err}`.red.bold);
+  server.close(() => process.exit(1));
 });
